ba89be017dd150a3b6004c14b0ba5940c641b947
Tomasz Omeljaniuk <tomekomel@gmail.com>
2019-02-21 10:38:26 +0100

    Req, res cata

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  console.log("Hey");
+  const tom = { name: 'Tom', age: 36, cool: true };
+  //res.send('Hey! It works!');
+  res.json(tom);
+});
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;
