ccccbb5edd3e5bcbda9fbc97077522db5686788d
Tomasz Omeljaniuk <tomekomel@gmail.com>
2019-02-25 15:32:42 +0100

    Routing fix

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,9 @@ const { catchErrors } = require('../handlers/errorHandlers');
 // Do work here
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
+router.get('/add', catchErrors(storeController.addStore));
 router.post('/add', catchErrors(storeController.createStore));
-router.post('/add/:id', catchErrors(storeController.updateStore));
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
+router.post('/add/:id', catchErrors(storeController.updateStore));
 
 module.exports = router;
