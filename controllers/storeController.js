bce4f463ca32f56298e55fc6ae0144ecdea88835
Tomasz Omeljaniuk <tomekomel@gmail.com>
2019-02-25 12:38:53 +0100

    Implement saving store

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -10,17 +10,8 @@ exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
 };
 
-exports.createStore = (req, res) => {
+exports.createStore = async (req, res) => {
   const store = new Store(req.body);
-  store
-    .save()
-    .then(store => {
-      return Store.find();
-    })
-    .then(stores => {
-      res.render('storeList', { stores: stores })
-    })
-    .catch(err => {
-      throw Error(err);
-    });
+  await store.save();
+  res.redirect('/');
 };
