d8eeecf7d3baa6023f7bbb6dabb148323896d09c
Tomasz Omeljaniuk <tomekomel@gmail.com>
2019-02-26 17:14:48 +0100

    Add tags page

--- routes/index.js
+++ routes/index.js
@@ -23,4 +23,7 @@ router.post('/add/:id',
 
 router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
+router.get('/tags', catchErrors(storeController.getStoresByTag));
+router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+
 module.exports = router;
